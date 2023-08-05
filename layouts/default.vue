9ee8fa0c8b8d82b70e0bd161571b6f9e1b5e3f7f
Nutchaya Varasit <nutchaya.0000@gmail.com>
2023-08-05 22:07:22 +0700

    Project added

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4 flex justify-between">
+                <NuxtLink to="/" class="font-bold">Archons</NuxtLink>
+                <ul class="flex gap-4">
+                    <li><NuxtLink to="/Barbatos" class="btn">Barbatos</NuxtLink></li>
+                    <li><NuxtLink to="/Beezebul" class="btn">Beezebul</NuxtLink></li>                    
+                    <li><NuxtLink to="/Morax" class="btn">morax</NuxtLink></li>
+                    <li><NuxtLink to="/Buer" class="btn">Buer</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+
+        <!-- page content go here -->
+        <div class="container mx-auto p-4">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .router-link-exact-active {
+        color: #12b488;
+    }
+</style>
